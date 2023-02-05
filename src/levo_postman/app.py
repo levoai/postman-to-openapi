fc0ccc69e4b54c004ca4ecb55ea7a9c11d6535b3
Buchi Reddy Busi Reddy <buchi@levo.ai>
2023-02-05 01:49:22 -0800

    Fixing a bug due to the way temp dir was used

--- src/levo_postman/app.py
+++ src/levo_postman/app.py
@@ -18,13 +18,14 @@ import asyncio
 import os
 import tempfile
 
+import aiofiles as aiofiles
 import uvicorn as uvicorn
-from fastapi import APIRouter, FastAPI, File, UploadFile, status
+from fastapi import APIRouter, Depends, FastAPI, File, UploadFile, status
 from fastapi.responses import FileResponse
 
-from .enrich_openapi_spec import enrich_spec
-from .oas_to_postman import convert_to_postman
-from .postman_collection_to_openapi import convert_to_openapi
+from levo_postman.enrich_openapi_spec import enrich_spec
+from levo_postman.oas_to_postman import convert_to_postman
+from levo_postman.postman_collection_to_openapi import convert_to_openapi
 
 CHUNK_SIZE = 1024 * 1024  # = 1MB
 
@@ -36,51 +37,52 @@ def health_check():
     return dict(message="I am doing good!")
 
 
-def _iter_file(file_path):
-    with open(file_path, mode="rb") as file_like:
-        yield from file_like
+async def get_temp_dir():
+    tmp_dir = tempfile.TemporaryDirectory()
+    try:
+        yield tmp_dir.name
+    finally:
+        del tmp_dir
 
 
 @router.post("/postman/to-oas")
-async def to_oas(file: UploadFile = File(...)):
-    with tempfile.TemporaryDirectory() as temp_dir:
-        tmp = tempfile.NamedTemporaryFile(delete=False)
-        try:
-            tmp.write(file.file.read())
-            file_path = tmp.name
+async def to_oas(file: UploadFile = File(...), temp_dir=Depends(get_temp_dir)):
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    try:
+        tmp.write(file.file.read())
+        file_path = tmp.name
 
-            oas_file_path = await convert_to_openapi(file_path, temp_dir)
-            oas_file_path = await enrich_spec(oas_file_path)
-            return FileResponse(
-                path=oas_file_path,
-                status_code=status.HTTP_201_CREATED,
-                filename=f"{file.filename}.yaml",
-            )
-        finally:
-            tmp.close()
-            os.unlink(tmp.name)
+        oas_file_path = await convert_to_openapi(file_path, temp_dir)
+        oas_file_path = await enrich_spec(oas_file_path)
+        return FileResponse(
+            path=oas_file_path,
+            status_code=status.HTTP_201_CREATED,
+            filename=f"{file.filename}.yaml",
+        )
+    finally:
+        tmp.close()
+        os.unlink(tmp.name)
 
 
 @router.post("/postman/from-oas")
-async def from_oas(file: UploadFile = File(...)):
-    with tempfile.TemporaryDirectory() as temp_dir:
-        tmp = tempfile.NamedTemporaryFile(delete=False)
-        try:
-            tmp.write(file.file.read())
-            file_path = tmp.name
+async def from_oas(file: UploadFile = File(...), temp_dir=Depends(get_temp_dir)):
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    try:
+        tmp.write(file.file.read())
+        file_path = tmp.name
 
-            postman_file_path = await convert_to_postman(file_path, temp_dir)
-            if os.stat(postman_file_path).st_size == 0:
-                raise Exception('Failed to convert OpenApi spec to Postman collection')
+        postman_file_path = await convert_to_postman(file_path, temp_dir)
+        if os.stat(postman_file_path).st_size == 0:
+            raise Exception('Failed to convert OpenApi spec to Postman collection')
 
-            return FileResponse(
-                path=postman_file_path,
-                status_code=status.HTTP_201_CREATED,
-                filename=f"collection.json",
-            )
-        finally:
-            tmp.close()
-            os.unlink(tmp.name)
+        return FileResponse(
+            path=postman_file_path,
+            status_code=status.HTTP_201_CREATED,
+            filename=f"collection.json",
+        )
+    finally:
+        tmp.close()
+        os.unlink(tmp.name)
 
 
 async def run_server():
@@ -89,6 +91,8 @@ async def run_server():
     # we need to override install_signal_handlers here because the existing implementation fails if
     # it isn't running in the main thread and uvicorn doesn't expose a way to configure it.
     server.install_signal_handlers = lambda: None
+
+    assert aiofiles is not None, "'aiofiles' must be installed to use FileResponse"
     await server.serve()
 
 
